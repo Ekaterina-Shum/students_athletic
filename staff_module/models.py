0a12c4da356277688cda454a0cb36f98fa346d93
Ekaterina-Shum <katyushaivanusha888@gmail.com>
2024-11-30 18:34:47 +0300

    Добавлены модели SportEvent и ParticipantsSportEvent  для хранения информации о спортивных мероприятиях и их участниках из университета среди студентов

--- staff_module/models.py
+++ staff_module/models.py
@@ -10,6 +10,35 @@ class Staff(models.Model):
         return f"{self.user.first_name} {self.user.last_name}"
 
 
+class SportEvent(models.Model):
+    TYPE_CHOICES = [
+        ('solo', 'Индивидуальное'),
+        ('team', 'Командное'),
+    ]
+
+    POSITION_CHOICES = [
+        ('1st', '1-Место'),
+        ('2nd', '2-Место'),
+        ('3rd', '3-Место'),
+        ('participant', 'Участник'),
+    ]
+
+    RESULT_CHOICES = [
+        ('win', 'Победа'),
+        ('defeat', 'Поражение'),
+        ('draw', 'Ничья')
+    ]
+    type = models.CharField(max_length=30, choices=TYPE_CHOICES, blank=True)
+    sport = models.ForeignKey("core.Sports", verbose_name=_("Вид спорта"), on_delete=models.CASCADE)
+    type_event = models.ForeignKey("core.SportCategory", verbose_name=_("Тип мероприятия"), on_delete=models.CASCADE)
+    result = models.CharField(max_length=30, choices=RESULT_CHOICES, blank=True)
+    position = models.CharField(max_length=30, choices=POSITION_CHOICES, blank=True, null=True)
+
+class ParticipantsSportEvent(models.Model):
+    event = models.ForeignKey(SportEvent, verbose_name=_("Мероприятие"), on_delete=models.CASCADE)
+    student = models.ForeignKey("core.Student", verbose_name=_("Студент"), on_delete=models.CASCADE, related_name="participants")
+    mvp = models.BooleanField(_("МВП"), default=False)
+
 class RequestSportAchievement(models.Model):
     STATUS_CHOICES = [
         ('created', 'Создано'),
