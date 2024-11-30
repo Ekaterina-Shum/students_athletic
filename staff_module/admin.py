7f37f4962740cc4fac1c965985d1decd90189b62
Ekaterina-Shum <katyushaivanusha888@gmail.com>
2024-11-30 17:18:32 +0300

    Созданы модели для хранения заявок и прикрепленных файлов к заявке; Модели RequestSportAchievement и RequestFiles добавлены в административную часть Django

--- staff_module/admin.py
+++ staff_module/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import RequestSportAchievement, RequestFiles
 
-# Register your models here.
+@admin.register(RequestSportAchievement)
+class RequestSportAchievementAdmin(admin.ModelAdmin):
+    list_display = ['student']
+
+@admin.register(RequestFiles)
+class RequestFilesAdmin(admin.ModelAdmin):
+    list_display = ['request', 'uploaded_at']
--- staff_module/models.py
+++ staff_module/models.py
@@ -1,4 +1,6 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
+import os
 from core.models import User
 
 class Staff(models.Model):
@@ -6,3 +8,24 @@ class Staff(models.Model):
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
+    
+
+class RequestSportAchievement(models.Model):
+    student = models.ForeignKey("core.Student", verbose_name=_("Студент"), on_delete=models.CASCADE, related_name="request")
+
+    def __str__(self):
+        return f"Запрос от {self.student}"
+
+
+class RequestFiles(models.Model):
+    request = models.ForeignKey(RequestSportAchievement, verbose_name=_("Запрос"), on_delete=models.CASCADE, related_name="file")
+    file = models.FileField(upload_to='request_files/',verbose_name=_("Файл"),blank=True,null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Дата загрузки"))
+
+    def __str__(self):
+        return f"Файл запроса: {self.request}"
+    
+    def delete(self, *args, **kwargs):
+        if self.file and os.path.isfile(self.file.path):
+            os.remove(self.file.path)
+        super().delete(*args, **kwargs)
